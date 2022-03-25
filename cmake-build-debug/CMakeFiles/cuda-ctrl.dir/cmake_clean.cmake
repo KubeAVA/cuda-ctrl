b9e8187179fca329d48d9264ecc5b60cf0340376
PTTQ <luorongzhou20@otcaix.iscas.ac.cn>
2022-03-25 16:17:12 +0800

    add code

---
+++ cmake-build-debug/CMakeFiles/cuda-ctrl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cuda-ctrl.dir/src/cuda_entries.cpp.o"
+  "libcuda-ctrl.pdb"
+  "libcuda-ctrl.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cuda-ctrl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- include/cuda-defines.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by win on 2022/3/23.
-//
-
-#ifndef CUDA_CTRL_CUDA_DEFINES_H
-#define CUDA_CTRL_CUDA_DEFINES_H
-
-#endif //CUDA_CTRL_CUDA_DEFINES_H
